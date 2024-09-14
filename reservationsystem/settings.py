2bf40b5d0d67bf564411449f23715545bc7465a4
Sudip <minigreed123@gmail.com>
2024-09-14 18:32:21 +0545

    ViP SECTION

--- reservationsystem/settings.py
+++ reservationsystem/settings.py
@@ -117,12 +117,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+    STATICFILES_DIRS =[
+    BASE_DIR, "static",
+] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 # Email backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
